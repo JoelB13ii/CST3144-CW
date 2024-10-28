6ace29ad30e33c200ff7266d67e56d5ecfc7b365
Joel Binny <joelbinny2003@gmail.com>
2024-10-28 11:18:41 +0400

    Frontend commit

---
+++ activities.js
@@ -0,0 +1,131 @@
+const activities = [
+    {
+        id: 1,
+        title: "Art Class",
+        description: "Learn to paint and create beautiful art.",
+        price: 100,
+        image: "art.jpg",
+        availableInventory: 4,
+        rating: 4,
+        location: "Downtown dubai"
+    },
+    {
+        id: 2,
+        title: "Dance Class",
+        description: "Join our fun dance classes for all ages.",
+        price: 120,
+        image: "dance.jpg",
+        availableInventory: 5,
+        rating: 5,
+        location: "Deira"
+    },
+    {
+        id: 3,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 150,
+        image: "music.jpg",
+        availableInventory: 2,
+        rating: 3,
+        location: "Sharjah"
+    },
+    
+    {
+        id: 4,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 150,
+        image: "music.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Abu dhabi"
+    },
+
+    {
+        id: 5,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 150,
+        image: "music.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Ras Al Khaimah"
+    },
+    {
+        id: 6,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 150,
+        image: "music.jpg",
+        availableInventory: 10,
+        rating: 3,
+        location: "JVC"
+    },
+
+    {
+        id: 7,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 150,
+        image: "music.jpg",
+        availableInventory: 4,
+        rating: 3,
+        location: "DSF"
+    },
+
+    {
+        id: 8,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 60,
+        image: "music.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Marina"
+        
+    },
+
+    {
+        id: 9,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 10,
+        image: "music.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Warqa"
+    },
+
+    {
+        id: 10,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 50,
+        image: "music.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Dubai Hills"
+    },
+
+    {
+        id: 11,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 120,
+        image: "music.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: "Bur Dubai"
+    },
+
+    {
+        id: 12,
+        title: "Music Lessons",
+        description: "Learn to play your favorite instrument.",
+        price: 130,
+        image: "music.jpg",
+        availableInventory: 5,
+        rating: 3,
+        location: " Barsha"
+    }
+];
